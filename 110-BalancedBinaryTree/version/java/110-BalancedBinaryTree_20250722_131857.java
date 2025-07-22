1a1938c2afd1698f4563b40cd4463eb2cb816dcc
nisvalpatel <80929417+nisvalpatel@users.noreply.github.com>
2025-07-22 13:18:57 -0400

    Create 110-BalancedBinaryTree

---
+++ 110-BalancedBinaryTree/version/java/110-BalancedBinaryTree_20250722_131857.java
@@ -0,0 +1,43 @@
+// Last updated: 7/22/2025, 1:18:57 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }i nu
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        int temp = recursionFunc(root);
+        if (temp == -1){
+            return false;
+        }
+        return true;
+    }
+
+    private int recursionFunc(TreeNode root){
+        if (root == null){return 0;}
+        // base case
+        if (root.left == null && root.right == null){
+            return 1;
+        }
+
+        int tempLeft = recursionFunc(root.left);
+        int tempRight = recursionFunc(root.right);
+        if (tempLeft == -1 || tempRight == -1 || Math.abs(tempRight - tempLeft) > 1 ){
+            return -1;
+        } else if (tempRight > tempLeft){
+            return (tempRight + 1);
+        } else{
+            return (tempLeft + 1);
+        }
+    }
+}
