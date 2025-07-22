3ea9e61374a82ce3f93c1aae3d4421609194477f
nisvalpatel <80929417+nisvalpatel@users.noreply.github.com>
2025-07-22 14:33:58 -0400

    Create 141-LinkedListCycle

---
+++ 141-LinkedListCycle/version/java/141-LinkedListCycle_20250722_143357.java
@@ -0,0 +1,33 @@
+// Last updated: 7/22/2025, 2:33:57 PM
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null){return false;}
+        ListNode fastPointer = head;
+        ListNode slowPointer = head;
+        
+        while (true){
+            for (int i = 0; i < 2; i++){
+                if (fastPointer.next == null){
+                    return false;
+                }
+                fastPointer = fastPointer.next;
+            }
+            slowPointer = slowPointer.next;
+            if (fastPointer == null){
+                return false;
+            }
+            if (slowPointer == fastPointer){return true;}
+        }
+    }
+}
