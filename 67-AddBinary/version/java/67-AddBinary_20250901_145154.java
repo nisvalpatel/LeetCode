87ed9c9bd8580d4fec3ebf0ac59a58e3b6747f7d
nisvalpatel <80929417+nisvalpatel@users.noreply.github.com>
2025-09-01 14:51:56 -0500

    Create 67-AddBinary

---
+++ 67-AddBinary/version/java/67-AddBinary_20250901_145154.java
@@ -0,0 +1,42 @@
+// Last updated: 9/1/2025, 2:51:54 PM
+class Solution {
+
+    public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int temp;
+        int A;
+        int B;
+
+
+        while (i >= 0 || j >= 0 || carry == 1){
+            if (i < 0 && j < 0 ){
+                sb.append(carry);
+                carry = 0;
+
+            } else if (i < 0){
+                temp = (b.charAt(j) - '0') + carry;
+                sb.append(temp%2);
+                carry = temp / 2;
+
+            }else if (j < 0){
+                temp = (a.charAt(i) - '0') + carry;
+                sb.append(temp%2);
+                carry = temp / 2;
+
+            }else{
+                A = a.charAt(i) - '0';
+                B = b.charAt(j) - '0';
+                temp = A + B + carry;
+                sb.append(temp%2);
+                carry = temp / 2;
+            }
+            i -= 1;
+            j -= 1;
+        }
+        return sb.reverse().toString();
+
+    }
+}
